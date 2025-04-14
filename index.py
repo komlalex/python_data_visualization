c9ec9e1b31ff39c4951c5e30c5bfc2dd7cc8ae5b
Alexander Komla <kunalexander360@gmail.com>
2025-04-14 21:47:30 +0000

    Markers and styling

--- index.py
+++ index.py
@@ -34,15 +34,15 @@ plot would be more informative if we could show the year for which the data
 is being plotted. We can do this by two arguments plt.plot"""
 years = [2010, 2011, 2012, 2013, 2014, 2015]
 yield_apples = [0.895, 0.91, 0.919, 0.926, 0.929, 0.931]   
-plt.figure()
-plt.plot(years, yield_apples) 
+#plt.figure()
+#plt.plot(years, yield_apples) 
 
 """Axis Labels 
 We can add labels to the axes to show what each axis represents using the 
 plt.xlabel and plt.ylabel methods. 
 """
-plt.xlabel("Year") 
-plt.ylabel("Yield (tons per hectare)") 
+#plt.xlabel("Year") 
+#plt.ylabel("Yield (tons per hectare)") 
 
 """Plotting Multiple Lines
 It's really easy to plot multiple lines in the same graph. Just invoke the 
@@ -53,17 +53,54 @@ years = range(2000, 2012)
 apples = [0.895, 0.91, 0.919, 0.926, 0.929, 0.931, 0.934, 0.936, 0.937, 0.9372, 0.939, 0.9342] 
 oranges = [0.962, 0.941, 0.930, 0.923, 0.918, 0.907, 0.904, 0.901, 0.898, 0.9, 0.896, 0.893] 
 
-plt.figure()
-plt.plot(years, apples) 
-plt.plot(years, oranges) 
-plt.ylabel("Yield (tons per hectare)")
-plt.xlabel("Year")
+#plt.figure()
+#plt.plot(years, apples) 
+#plt.plot(years, oranges) 
+#plt.ylabel("Yield (tons per hectare)")
+#plt.xlabel("Year")
 
 """Chart Title and Legend 
 To differentiate between multiple lines, we can include a legend 
 within the graph using the plt.legend function. We also give the entire 
 chart a title using the plt.title function. 
 """ 
+#plt.title("Crop Yields in Kanto")
+#plt.legend(["Apples", "Oranges"]) 
+
+
+"""Line Markers 
+We can also show markers for data points on each line using the marker 
+argument of plt.plot. Matplotlib supports many different types of markers like 
+circles, squares, cross, diamond etc.  
+
+Styling Lines and Markers  
+The plt.plot function supports many arguments for styling lines and markers: 
+* color or c: set the color of the line 
+* linestyle or ls: choose between a solid or dashed line 
+* linewidth or lw: set the width of a line 
+* markersize or ms: set the size of the markers 
+* markeredgecolor or mec: set the edge color for markers 
+* markerfacecolor or mfc: set the fill color for markers 
+* alpha: opacity of the plot
+"""
+plt.figure()
+plt.plot(years, apples, marker="o", c="b", ls="-", lw=2, ms=8, mew=2, mec= "navy") 
+plt.plot(years, oranges, marker="x", c="r", ls="--", lw=3, ms=10, alpha=0.5) 
+plt.ylabel("Yield (tons per hectare)")
+plt.xlabel("Year") 
+plt.title("Crop Yields in Kanto")
+plt.legend(["Apples", "Oranges"]) 
+
+"""The fmt argument provies a shorthand for specifying the line style, 
+marker and line color. It can be provided as the third argument
+
+fmt = '[marker][line][color]'
+"""
+plt.figure()
+plt.plot(years, apples, "s-b") 
+plt.plot(years, oranges, "o--r") 
+plt.ylabel("Yield (tons per hectare)")
+plt.xlabel("Year") 
 plt.title("Crop Yields in Kanto")
 plt.legend(["Apples", "Oranges"]) 
 
